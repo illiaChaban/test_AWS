c2d2085c6e9da849b0e6c7455e3bd25a7756d6d3
Illia Chaban <illia.chaban.8@gmail.com>
2018-07-12 16:29:07 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+app.use(express.static('static'))
+
+const server = http.createServer(app)
+
+const port = 3000;
+console.log(`HTTP server running at http://localhost:${port}`);
+server.listen(port);
